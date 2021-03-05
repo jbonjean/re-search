e2e477ce14d3893da384d8a6cf9a9649e54b94cb
Marco Herrn <marco@mherrn.de>
2021-03-05 11:14:02 +0100

    Removed duplicate append_to_history() method
    append_to_history() was also introduced with commit
    c224a91976ea8817c9e6e4c47c43ca7d52c19f73 and has a better implementation
    as it does not error out if the maximum history size was reached.

--- re-search.c
+++ re-search.c
@@ -280,23 +280,6 @@ void execute(char *cmdline) {
 	set_input_mode();
 }
 
-void append_to_history(char *cmdline) {
-	if (history_size >= MAX_HISTORY_SIZE) {
-		error("too many history entries");
-		return;
-	}
-
-	int len = strlen(cmdline);
-	if (!malloc(len + 1)) {
-		error("cannot allocate memory");
-		return;
-	}
-
-	history[history_size] = malloc(len + 1);
-	strncpy(history[history_size], cmdline, len + 1);
-	history_size++;
-}
-
 void restore_terminal() {
 	// reset color
 	fprintf(stderr, "%s", NORMAL);
